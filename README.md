61a940c5db1c33068330271ebd01f4ae4dabdfd0
Natalia Meneses <nmeneses8@gmail.com>
2021-02-09 16:10:44 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aplicación de Clima por consola Node
+
+Esta es una aplicación por consola hecha en Node sobre el clima.
+
+```
+npm install
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+
+const { inquirerMenu, pausa, leerInput, listarLugares } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
+
+const main = async() => {
+    
+    const busquedas = new Busquedas();
+    let opt;
+
+    do {
+        // Función para imprimir menú
+        opt = await inquirerMenu();
+        
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const termino = await leerInput('Ciudad: ');
+                
+                // Buscar los lugares
+                const lugares = await busquedas.ciudad(termino);
+                
+                // Selecciona el lugar
+                const id = await listarLugares(lugares);
+                if( id === '0' ) continue;
+
+                const lugarSel = lugares.find( l => l.id === id );
+
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
+
+                // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+                // Mostrar resultados
+
+                console.clear();
+                console.log('\nInformación de la ciudad \n'.green);
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura:', clima.temp);
+                console.log('Mínima:', clima.min);
+                console.log('Máxima:', clima.max);
+                console.log('Como esta el clima:', clima.desc.green);
+            break;
+            
+            case 2:
+                // Mostrar historial busqueda
+                busquedas.historialCapitalizado.forEach( (lugar, i) => {
+                    const idx = `${ i + 1}.`.green;
+                    console.log(`${idx} ${lugar}`)
+                })
+            break;
+
+            case 0:
+                // Salir
+                console.log(opt)
+            break;
+        }
+
+        if(opt !== 0) await pausa();
+    } while ( opt !== 0)
+}
+
+main();
